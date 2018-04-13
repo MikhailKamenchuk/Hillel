67bf54b0ccac167a69a9a741b86738b5fc9614f2
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2018-04-13 14:50:09 +0300

    Adding h.w.#7

---
+++ h.w.#7/js/script.js
@@ -0,0 +1,51 @@
+//Функция, которая принимает функцию и аргументы и возвращает результат
+
+function sum() {
+  return [].reduce.call(arguments, function(a, b) {
+    return a + b;
+  });
+}
+
+function mul() {
+  return [].reduce.call(arguments, function(a, b) {
+    return a * b;
+  });
+}
+
+function sub() {
+  return [].reduce.call(arguments, function(a, b) {
+    return a - b;
+  });
+}
+
+function div() {
+  return [].reduce.call(arguments, function(a, b) {
+    return a / b;
+  });
+}
+
+function applyAll(func) {
+  return func.apply(this, [].slice.call(arguments, 1));
+}
+alert(applyAll(sum, 8, 4, 2));
+alert(applyAll(mul, 8, 4, 2));
+alert(applyAll(sub, 8, 4, 2));
+alert(applyAll(div, 8, 4, 2));
+
+
+//Функция Фибоначчи
+
+let ask = +prompt('Введите число Фибоначчи', '5')
+
+function fibonacci(n) {
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  };
+  return b;
+};
+
+alert(fibonacci(ask));
