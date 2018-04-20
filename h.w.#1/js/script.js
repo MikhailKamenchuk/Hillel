6f7aea26d93788fca112e23134aa0c767a4797e9
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2018-04-20 15:41:38 +0300

    Adding h.w.#1

---
+++ h.w.#1/js/script.js
@@ -0,0 +1,14 @@
+1.  Написать 5-ть вариантов записи строки: It's beautiful day. I like "free day".
+'It\'s beautiful day. I like "free day"'
+"It's beautiful day. I like \"free day\""
+'It\'s beautiful day. I like \'free day\' '
+"It's beautiful day. I like  'free day ' "
+'It\'s beautiful day. I like \u2033 free day \u2033 '
+
+2.  Написать, правильное умножение двух дробных чисел
+let exp = 5.45 * 100 * 8.3 * 100 / 10000;
+console.log(exp)
+
+3. "string" + 2 => NaN исправить :)
+ 	+"string" + 2
+
