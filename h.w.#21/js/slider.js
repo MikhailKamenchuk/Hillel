eaac7fda69e84891c80a1f71d3913ce9c10d394c
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2018-05-24 08:36:07 +0300

    Adding h.w.#21

---
+++ h.w.#21/js/slider.js
@@ -0,0 +1,17 @@
+function Slidebox(name){
+  var container = document.getElementById(name)
+  var imgs = container.children;
+  var len = imgs.length;
+  var i = len - 1;
+  return function go(){
+     imgs[i].classList.remove('active')
+     i = ++i % len;
+    // console.log(i);
+     imgs[i].classList.add('active');
+     var timerId = window.setTimeout(go, 1000)
+  }
+}
+
+let firstSlider = new Slidebox('slider');
+firstSlider();
+
