a689660057ea3fd6dd1f1447781022be46b27a77
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2018-05-24 22:52:46 +0300

    Completed first slider in h.w.#16

--- h.w.#16/js/slider.js
+++ h.w.#16/js/slider.js
@@ -1,21 +1,58 @@
-function Slidebox(name){
-  var container = document.getElementById(name)
-  var imgs = container.children;
-  var len = imgs.length;
-  var i = len - 1;
-  (function go(){
-     imgs[i].classList.remove('active')
-     i = ++i % len;
-    console.log(i);
-     imgs[i].classList.add('active');
-     var timerId = window.setTimeout(go, 1000)
-  })();
+function Slidebox(parent, forItems){
+  this.container = document.querySelector(parent);
+  let items = document.querySelectorAll(forItems);
+  let counter = 0; 
+  let timerId;
+  this.currentElem = null;
+  this.go = function(){
+        items[counter].classList.remove('show');
+        counter++;
+        counter = counter < 0 ? items.length - 1 : counter == items.length ? 0 : counter;
+        items[counter].classList.add('show'); 
+  }
+  this.start = function (){
+    timerId = setInterval(this.go, 1000);
+  }
+  this.stop = function (){
+    clearTimeout(timerId);
+  }
+   
 }
 
-var firstSlider = new Slidebox('first-slider');
 
+var firstSlider = new Slidebox('#first-slider', '#first-slider figure');
+window.addEventListener('load', firstSlider.start());
 
-var anotherSlider = new Slidebox('another-slider');
+firstSlider.container.onmouseover = function(event){
+  if(firstSlider.currentElem){
+    return;
+  }
+  let figure = event.target;
+  while (figure != this) {
+    if (figure.tagName == 'FIGURE') break;
+    figure = figure.parentNode;
+  }
+  if (figure == this) return;
+  firstSlider.stop();
+  firstSlider.currentElem = figure;
+  figure.lastElementChild.style.display = "block";
+}
 
+firstSlider.container.onmouseout = function(event){
+  if (!firstSlider.currentElem) return;
+  var relatedTarget = event.relatedTarget;
+  if(relatedTarget){
+    while(relatedTarget){
+    if (relatedTarget == firstSlider.currentElem) return;
+      relatedTarget = relatedTarget.parentNode;
+    }
+  }
+  firstSlider.start();
+  firstSlider.currentElem.lastElementChild.style.display = '';
+  firstSlider.currentElem = null;
+}
+
+var anotherSlider = new Slidebox('#another-slider', '#another-slider figure');
+window.addEventListener('load', anotherSlider.start());
 
 
